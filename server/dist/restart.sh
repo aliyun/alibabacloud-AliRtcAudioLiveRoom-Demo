5259460bc1bb3c4816f2efa3a7955753cb8bc0a8
术专 <burui.br@alibaba-inc.com>
2020-12-04 14:52:12 +0800

    code update

--- server/dist/restart.sh
+++ server/dist/restart.sh
@@ -2,4 +2,5 @@
 
 lsof -i:8080 | grep java | awk '{print $2}' | xargs kill -9
 
-nohup java org.springframework.boot.loader.JarLauncher >/dev/null 2>&1 &
+#nohup java org.springframework.boot.loader.JarLauncher >/dev/null 2>&1 &
+java org.springframework.boot.loader.JarLauncher
